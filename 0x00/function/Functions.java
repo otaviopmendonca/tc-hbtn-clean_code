de8c22bf6dfd45374796be03a20dfc57ab82e013
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-08-13 11:01:31 -0300

    refactor: corrige exercicio Somente 1 nivel de abstracao

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -9,10 +9,10 @@ public class Functions {
     }
 
     //Somente 1 nível de abstração
-    //B
+    //A
     @PostMapping("/user")
     public User saveUser(User user){
-        return userService.userRepository.save(user);
+        return userService.save(user);
     }
 
     // Arguments
